0d9b1d24ac88d79c0970f01d2198afb14fd31b88
Your Name <lucassimpson0213@gmail.com>
2024-12-18 17:18:44 +0000

    Add database loggin

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
 const express = require('express');
 const server = require('http').createServer();
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database(':memory:');
+const createTable = () => {
+    db.run(`
+        CREATE TABLE visitors (
+        count INTEGER, 
+        time TEXT
+    ) `);
 
+}
+
+const insertData = (data) => {
+    db.run('SELECT * FROM visitors;')
+}
+
+const getData = () => {
+    db.each('SELECT * FROM visitors;', (err, row) => {
+        console.log(row);
+    })
+}
+
+function shutdownDB() {
+    getData();
+    console.log("shutting down");
+    db.close();
+}
 const app = express();
+createTable();
 
 app.get('/', function(req, res) {
 res.sendFile('index.html', {root: __dirname});
@@ -12,6 +38,14 @@ server.on('request', app)
 
 
 server.listen(3000, function () { console.log("listening on 3000"); });
+wss.clients.forEach(function each(client) {
+    client.close();
+})
+process.on('SIGINT', () => {
+    server.close(() => {
+        shutdownDB();
+    })
+})
 
 
 /** Start WebSockets **/
@@ -21,16 +55,19 @@ const wss = new WebSocketServer({ server: server });
 
 wss.on('connection', function (ws) {
     const numClients = wss.clients.size;
-    console.log("clients connected:", numClients);
+    console.log("clients fuckkkk connected:", numClients);
     wss.broadcast(`current visitors: ${numClients}`);
 
     if (ws.readyState === ws.OPEN) {
         ws.send('Welcome to my server');
     }
 
+    db.run(`INSERT INTO visitors (count time)
+        VALUES(${numClients} datetime('now'))`);
+
     ws.on('close', function() {
         const updatedClients = wss.clients.size; // Recalculate the client count
-        wss.broadcast(`current visitors: ${updatedClients}`);
+        wss.broadcast(`current visitorss: ${updatedClients}`);
         console.log("a client has disconnected");
     });
 });
@@ -43,4 +80,8 @@ wss.broadcast = function broadcast(data) {
         }
     });
 };
+
+
+
+
 
